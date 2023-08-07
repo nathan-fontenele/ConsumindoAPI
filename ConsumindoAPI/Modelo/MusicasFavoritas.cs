df2e353e1163dda3d1faa076e3f80e2ae580c66e
Nathan Fontenele <nathangf60@outlook.com>
2023-08-07 15:05:14 -0300

    Criação de classe para manipulação da lista de favoritos

---
+++ ConsumindoAPI/Modelo/MusicasFavoritas.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsumindoAPI.Modelo;
+
+public class MusicasFavoritas
+{
+   public string? Name { get; set; }
+   public List<ApiModel> Favoritas { get; }
+
+
+   public MusicasFavoritas(string nome)
+   {
+      Name = nome;
+      Favoritas = new List<ApiModel>();
+   }
+
+   public void add(ApiModel musica)
+   {
+      Favoritas.Add(musica);
+   }
+
+   public void getFavoriteMusics()
+   {
+      Console.WriteLine(Name);
+
+      foreach (var item in Favoritas)
+      {
+         Console.WriteLine(item.Musica);
+         Console.WriteLine(item.Artista);
+      }
+   }
+}
